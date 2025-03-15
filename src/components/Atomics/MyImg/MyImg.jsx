377ff5df370c73fcd9f6870362447ecfb2dd1985
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-03-15 18:13:11 -0500

    Modulo de inicio

---
+++ src/components/Atomics/MyImg/MyImg.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import foto from "../../../assets/img/foto.jpg"
+ export const MyImg = () => {
+  return (
+    <img className={` mt-[80px] mb-[15px] w-[200px] h-[200px] rounded-[50%]`} src={foto} alt="miFoto" />
+  )
+}
--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import { MyImg } from '../../Atomics/myImg/myImg'
  export const Home = () => {
   return (
-    <div>
-      <h1>inicio</h1>
+    <div className='flex flex-col items-center h-full text-center max-w-[580px]'>
+      <MyImg/>
+      <h1 className='text-white'>¡Hola! Soy Eusebio Suarez</h1>
+      <p className="text-white pt-5">
+      Desarrollador junior con conocimientos en backend y frontend, con un mayor enfoque en frontend.  
+      Actualmente, cursando el tecnólogo en Análisis y Desarrollo de Software, adquiriendo habilidades en el diseño, 
+      desarrollo e implementación de aplicaciones web y móviles.  
+      Apasionado por la creación de interfaces intuitivas y dinámicas, con interés en mejorar la experiencia del usuario a través de 
+      tecnologías modernas y buenas prácticas de desarrollo.
+      </p>
     </div>
   )
 }
