9364fb292a787d3511f816c41dfdf9728e64044c
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-03-16 20:58:58 -0500

    Correcion de desborde en el modulo inicio

--- src/components/Pages/Home/Home.jsx
+++ src/components/Pages/Home/Home.jsx
@@ -5,7 +5,7 @@ import { MyImg } from '../../Atomics/MyImg/MyImg'
     <div className='flex flex-col items-center text-center'>
       <MyImg/>
       <h1 className='text-white'>¡Hola! Soy Eusebio Suarez</h1>
-      <p className="text-white pt-5 max-w-[580px] m-4">
+      <p className="text-white pt-5 pb-[50px] px-4 ">
       Desarrollador junior con conocimientos en backend y frontend, con un mayor enfoque en frontend.  
       Actualmente, cursando el tecnólogo en Análisis y Desarrollo de Software, adquiriendo habilidades en el diseño, 
       desarrollo e implementación de aplicaciones web y móviles.  
