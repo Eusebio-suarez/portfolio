2a9f93496e3365c1e3be124051d555238a99da3d
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-05-07 14:59:51 -0500

    animacion en las imagenes de las targetas de habilidades

--- src/components/Layouts/TechCard/TechCard.jsx
+++ src/components/Layouts/TechCard/TechCard.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 export const TechCard = ({SrcImg = "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg", Name = "React", level = "Intermedio"}) => {
   return (
     <div className="bg-[#222d43] w-[130px] p-2 rounded-lg shadow-md text-white flex flex-col items-center justify-center
-      opacity-100 scale-95 animate-fadeIn transition-transform duration-600 hover:scale-105 hover:opacity-80">  
-        <img src={SrcImg} alt="logo" className="w-[50px] h-[50px] mx-auto"/>
+      opacity-100 scale-95 animate-fadeIn transition-transform duration-600 hover:scale-105 hover:opacity-80 skillContainer">  
+        <img src={SrcImg} alt="logo" className="skillImg w-[50px] h-[50px] mx-auto"/>
         <p className='pt-3'>{Name}</p>
         <p className='text-blue-500'>{level}</p>
     </div>
