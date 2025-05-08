9b2bbc3fecb9dc193a7257877f1e663a24aaff7c
Eusebio-suarez <eusebiosuaresmartines@gmail.com>
2025-05-08 06:41:10 -0500

    configurar responsive de el modulo proyectos

--- src/components/Layouts/Proyect/Proyect.jsx
+++ src/components/Layouts/Proyect/Proyect.jsx
@@ -9,11 +9,11 @@ export const Proyect = ({
   img = ProyectImg1
 }) => {
   return (
-    <div className="max-w-sm rounded-lg overflow-hidden shadow-lg transition-transform transform hover:translate-y-2 w-full sm:w-72 md:w-80 lg:w-96">
-      <img className="w-full h-48 object-cover hover:scale-105 transition duration-500" src={img} alt="Project Image"/>
-      <div className="p-6 bg-white">
+    <div className=" flex items-center flex-col max-w-sm rounded-t-lg overflow-hidden shadow-lg transition-transform transform hover:translate-y-2 w-full sm:w-72 md:w-80 lg:w-96">
+      <img className="w-[80%] h-40 object-cover hover:scale-[102%] transition duration-500 rounded-lg" src={img} alt="Project Image"/>
+      <div className="w-[80%] p-6 bg-white rounded-b-lg">
         <h3 className="text-xl font-bold text-gray-800 mb-2">{name}</h3>
-        <p className="text-gray-600 text-base mb-4">{desc}</p>
+        <p className="text-gray-600 text-sm mb-4 ">{desc}</p>
         <div className="flex gap-5 flex-wrap text-center">
           <a href={linkRep}className="inline-block bg-indigo-600 text-white px-4 py-2 rounded-lg hover:bg-indigo-700 transition-colors w-full sm:w-auto">Ver en GitHub</a>
           <a href={linkDesp} className="inline-block bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 transition-colors w-full sm:w-auto">Ver sitio Web</a>
